63cc67d213efdbe8b2f8457e06cd066fb3599939
Thiwanka Chanditha <thiwankasinhalage@ieee.org>
2024-10-28 20:53:00 +0530

    changes to the user and admin sidebar

--- utils/admin.php
+++ utils/admin.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 if(isset($_SESSION["user_type"])){
     if($_SESSION["user_type"] != "ADMIN" && $_SESSION["user_type"] != "SUPER_ADMIN"){
-        header("Location: ../home/user.home.view.php");
+        header("Location: ../dashboard/user.home.view.php");
         return;
     }
 }
